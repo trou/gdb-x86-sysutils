2208851e1f609d327286b048c17469d7ccd64062
Raphaël Rigo <raphael.rigo@eads.net>
2014-04-10 17:02:48 +0200

    TSS parser; base computation fix

--- gdb-utils.py
+++ gdb-utils.py
@@ -5,7 +5,7 @@ class segment_desc:
     def __init__(self, val):
         self.desc = val
 
-        self.base = (self.desc >> 16)&0xFFFFF
+        self.base = (self.desc >> 16)&0xFFFFFF
         self.base |= ((self.desc >> (32+24))&0xFFF)<<24;
 
         self.limit = self.desc & 0xFFFF;
@@ -38,6 +38,8 @@ class segment_desc:
             return ("DATA", s)
 
     def __str__(self):
+        if self.p == 0:
+            return "Not Present !"
         if self.s == 1:
             # CODE/DATA
             s = "DPL : %d Base : %08x Limit : %08x " % (self.dpl, self.base, self.limit)
@@ -50,8 +52,22 @@ class segment_desc:
             s += "Type: %s" % ("Reserved", "16b TSS (A)", "LDT", "16b TSS (B)", "16b Call G", "Task Gate", "16b Int G", "16b Trap G", "Reserved", "32b TSS (A)", "Reserved", "32b TSS (B)", "32b Call G", "Reserved", "32b Int G", "32b Trap G")[self.type]
         return s
 
+class tss_data:
+    def __init__(self, data):
+        if len(data) != 104:
+            raise Exception("toto")
+        self.raw = data
+        (self.ptl, _, self.esp0, self.ss0, _, self.esp1, self.ss1, _,
+                   self.esp2, self.ss2, _, self.cr3, self.eip, self.eflags, self.eax, self.ecx,
+                   self.edx, self.ebx, self.esp, self.ebp, self.esi, self.edi, self.es, _,
+                   self.cs, _, self.ss, _, self.ds, _, self.fs, _,
+                   self.gs, _, ldtss, _, t, iomap) = struct.unpack("HHIHHIHHIHH11I16H", data)
+
+    def __str__(self):
+        return "CR3 : %08x" % self.cr3
+
 class GdtCommand(gdb.Command):
-    "GDT command for GDT parsing"
+    "Commands for GDT parsing"
 
     def __init__ (self):
         super (GdtCommand, self).__init__ ("gdt",
@@ -66,6 +82,7 @@ class GdtDumpCommand(gdb.Command):
             gdb.COMMAND_SUPPORT,
             gdb.COMPLETE_NONE, True)
 
+    # TODO : add limit
     def invoke (self, arg, from_tty):
         ad = long(arg, 0)
         inf = gdb.selected_inferior()
@@ -73,6 +90,20 @@ class GdtDumpCommand(gdb.Command):
             desc = struct.unpack("<Q", inf.read_memory(ad+i*8, 8))[0]
             if desc != 0:
                 print "#%04d : %016x : %s" % (i,desc, segment_desc(desc))
+
+class GdtTssCommand(gdb.Command):
+    "Dump TSS"
+
+    def __init__ (self):
+        super (GdtTssCommand, self).__init__ ("gdt tss",
+            gdb.COMMAND_SUPPORT,
+            gdb.COMPLETE_NONE, True)
+
+    def invoke (self, arg, from_tty):
+        ad = long(arg, 0)
+        inf = gdb.selected_inferior()
+        sd = tss_data(inf.read_memory(ad, 104))
+        print sd
 
 class GdtDecodeCommand(gdb.Command):
     "Decode GDT descriptor"
@@ -89,3 +120,4 @@ class GdtDecodeCommand(gdb.Command):
 GdtCommand()
 GdtDecodeCommand()
 GdtDumpCommand()
+GdtTssCommand()
